93ac183e78401c5141331a6451538c2ca2cfc94c
Florent Teichteil-koenigsbuch <florent.teichteil-koenigsbuch@airbus.com>
2021-02-08 16:35:25 +0100

    Move MARTDP to the new C++ hub API

--- cpp/src/hub/py_skdecide.cc
+++ cpp/src/hub/py_skdecide.cc
@@ -12,11 +12,11 @@ void init_pyaostar(py::module& m);
 void init_pyastar(py::module& m);
 void init_pyilaostar(py::module& m);
 void init_pylrtdp(py::module& m);
+void init_pymartdp(py::module& m);
 void init_pymcts(py::module& m);
 // void init_pyiw(py::module& m);
 void init_pyriw(py::module& m);
 // void init_pybfws(py::module& m);
-// void init_pymartdp(py::module& m);
 
 PYBIND11_MODULE(__skdecide_hub_cpp, m) {
     skdecide::Globals::init();
@@ -24,9 +24,9 @@ PYBIND11_MODULE(__skdecide_hub_cpp, m) {
     init_pyastar(m);
     init_pyilaostar(m);
     init_pylrtdp(m);
+    init_pymartdp(m);
     init_pymcts(m);
     // init_pyiw(m);
     init_pyriw(m);
     // init_pybfws(m);
-    // init_pymartdp(m);
 }
