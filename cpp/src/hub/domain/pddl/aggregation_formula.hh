8c658240fc7133dbdda7f607298c0677a2e32356
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-18 13:39:00 +0200

    Break up aggregation formula code

--- cpp/src/hub/domain/pddl/aggregation_formula.hh
+++ cpp/src/hub/domain/pddl/aggregation_formula.hh
@@ -10,124 +10,76 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        template <typename Derived>
-        class AggregationFormula : public Formula {
-        public :
-            typedef std::shared_ptr<AggregationFormula<Derived>> Ptr;
-            typedef Formula::Ptr FormulaPtr;
-            typedef std::vector<Formula::Ptr> FormulaVector;
+template <typename Derived> class AggregationFormula : public Formula {
+public:
+  typedef std::shared_ptr<AggregationFormula<Derived>> Ptr;
+  typedef Formula::Ptr FormulaPtr;
+  typedef std::vector<Formula::Ptr> FormulaVector;
 
-            AggregationFormula() {}
-            
-            AggregationFormula(const AggregationFormula& other)
-                : _formulas(other._formulas) {}
-            
-            AggregationFormula& operator= (const AggregationFormula& other) {
-                this->_formulas = other._formulas;
-                return *this;
-            }
+  AggregationFormula();
+  AggregationFormula(const AggregationFormula &other);
+  AggregationFormula &operator=(const AggregationFormula &other);
+  virtual ~AggregationFormula();
 
-            virtual ~AggregationFormula() {}
+  AggregationFormula &append_formula(const Formula::Ptr &formula);
 
-            AggregationFormula& append_formula(const Formula::Ptr& formula) {
-                _formulas.push_back(formula);
-                return *this;
-            }
+  /**
+   * Removes the formula at a given index.
+   * Throws an exception if the given index exceeds the size of the
+   * aggregation formula
+   */
+  AggregationFormula &remove_formula(const std::size_t &index);
 
-            /**
-             * Removes the formula at a given index.
-             * Throws an exception if the given index exceeds the size of the
-             * aggregation formula
-             */
-            AggregationFormula& remove_formula(const std::size_t& index) {
-                if (index >= _formulas.size()) {
-                    throw std::out_of_range("SKDECIDE exception: index " + std::to_string(index) +
-                                            " exceeds the size of the '" + Derived::class_name + "' formula");
-                } else {
-                    _formulas.erase(_formulas.begin() + index);
-                    return *this;
-                }
-            }
+  /**
+   * Gets the formula at a given index.
+   * Throws an exception if the given index exceeds the size of the
+   * aggregation formula
+   */
+  const Formula::Ptr &formula_at(const std::size_t &index);
 
-            /**
-             * Gets the formula at a given index.
-             * Throws an exception if the given index exceeds the size of the
-             * aggregation formula
-             */
-            const Formula::Ptr& formula_at(const std::size_t& index) {
-                if (index >= _formulas.size()) {
-                    throw std::out_of_range("SKDECIDE exception: index " + std::to_string(index) +
-                                            " exceeds the size of the '" + Derived::class_name + "' formula");
-                } else {
-                    return _formulas[index];
-                }
-            }
+  const FormulaVector &get_formulas() const;
+  virtual std::ostream &print(std::ostream &o) const;
 
-            const FormulaVector& get_formulas() const {
-                return _formulas;
-            }
+private:
+  FormulaVector _formulas;
+};
 
-            virtual std::ostream& print(std::ostream& o) const {
-                o << "(" << Derived::class_name;
-                for (const auto& f : _formulas) {
-                    o << " " << *f;
-                }
-                o << ")";
-                return o;
-            }
-        
-        private :
-            FormulaVector _formulas;
-        };
+class ConjunctionFormula : public AggregationFormula<ConjunctionFormula> {
+public:
+  static constexpr char class_name[] = "and";
 
+  typedef std::shared_ptr<ConjunctionFormula> Ptr;
+  typedef AggregationFormula<ConjunctionFormula>::FormulaPtr FormulaPtr;
+  typedef std::vector<FormulaPtr> FormulaVector;
 
-        class ConjunctionFormula : public AggregationFormula<ConjunctionFormula> {
-        public :
-            static constexpr char class_name[] = "and";
+  ConjunctionFormula();
+  ConjunctionFormula(const ConjunctionFormula &other);
+  ConjunctionFormula &operator=(const ConjunctionFormula &other);
+  virtual ~ConjunctionFormula();
+};
 
-            typedef std::shared_ptr<ConjunctionFormula> Ptr;
-            typedef AggregationFormula<ConjunctionFormula>::FormulaPtr FormulaPtr;
-            typedef std::vector<FormulaPtr> FormulaVector;
+class DisjunctionFormula : public AggregationFormula<DisjunctionFormula> {
+public:
+  static constexpr char class_name[] = "or";
 
-            ConjunctionFormula() {}
-            
-            ConjunctionFormula(const ConjunctionFormula& other)
-                : AggregationFormula(other) {}
-            
-            ConjunctionFormula& operator= (const ConjunctionFormula& other) {
-                dynamic_cast<AggregationFormula<ConjunctionFormula>&>(*this) = other;
-                return *this;
-            }
+  typedef std::shared_ptr<DisjunctionFormula> Ptr;
+  typedef AggregationFormula<DisjunctionFormula>::FormulaPtr FormulaPtr;
+  typedef std::vector<FormulaPtr> FormulaVector;
 
-            virtual ~ConjunctionFormula() {}
-        };
+  DisjunctionFormula();
+  DisjunctionFormula(const DisjunctionFormula &other);
+  DisjunctionFormula &operator=(const DisjunctionFormula &other);
+  virtual ~DisjunctionFormula();
+};
 
-
-        class DisjunctionFormula : public AggregationFormula<DisjunctionFormula> {
-        public :
-            static constexpr char class_name[] = "or";
-
-            typedef std::shared_ptr<DisjunctionFormula> Ptr;
-            typedef AggregationFormula<DisjunctionFormula>::FormulaPtr FormulaPtr;
-            typedef std::vector<FormulaPtr> FormulaVector;
-
-            DisjunctionFormula() {}
-            
-            DisjunctionFormula(const DisjunctionFormula& other)
-                : AggregationFormula(other) {}
-            
-            DisjunctionFormula& operator= (const DisjunctionFormula& other) {
-                dynamic_cast<AggregationFormula<DisjunctionFormula>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~DisjunctionFormula() {}
-        };
-
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
+
+#ifdef SKDECIDE_HEADERS_ONLY
+#include "impl/aggregation_formula_impl.hh"
+#endif
 
 #endif // SKDECIDE_PDDL_AGGREGATION_FORMULA_HH
