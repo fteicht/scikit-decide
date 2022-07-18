c6ba3c6ea8a77d9f142a9c79ae4e0cf08a58ceb1
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-18 14:01:07 +0200

    Break up assignment effect code

--- cpp/src/hub/domain/pddl/assignment_effect.hh
+++ cpp/src/hub/domain/pddl/assignment_effect.hh
@@ -11,181 +11,109 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        template <typename Derived>
-        class AssignmentEffect : public Effect {
-        public :
-            typedef std::shared_ptr<AssignmentEffect<Derived>> Ptr;
+template <typename Derived> class AssignmentEffect : public Effect {
+public:
+  typedef std::shared_ptr<AssignmentEffect<Derived>> Ptr;
 
-            AssignmentEffect() {}
+  AssignmentEffect();
+  AssignmentEffect(const FunctionExpression::Ptr &function,
+                   const Expression::Ptr &expression);
+  AssignmentEffect(const AssignmentEffect<Derived> &other);
+  AssignmentEffect<Derived> &operator=(const AssignmentEffect<Derived> &other);
+  virtual ~AssignmentEffect();
 
-            AssignmentEffect(const FunctionExpression::Ptr& function,
-                             const Expression::Ptr& expression)
-                : _function(function), _expression(expression) {}
-            
-            AssignmentEffect(const AssignmentEffect<Derived>& other)
-                : _function(other._function), _expression(other._expression) {}
-            
-            AssignmentEffect<Derived>& operator= (const AssignmentEffect<Derived>& other) {
-                this->_function = other._function;
-                this->_expression = other._expression;
-                return *this;
-            }
+  void set_function(const FunctionExpression::Ptr &function);
+  const FunctionExpression::Ptr &get_function() const;
 
-            void set_function(const FunctionExpression::Ptr& function) {
-                _function = function;
-            }
+  void set_expression(const Expression::Ptr &expression);
+  const Expression::Ptr &get_expression() const;
 
-            const FunctionExpression::Ptr& get_function() const {
-                return _function;
-            }
+  virtual std::ostream &print(std::ostream &o) const;
+  std::string print() const;
 
-            void set_expression(const Expression::Ptr& expression) {
-                _expression = expression;
-            }
+private:
+  FunctionExpression::Ptr _function;
+  Expression::Ptr _expression;
+};
 
-            const Expression::Ptr& get_expression() const {
-                return _expression;
-            }
+class AssignEffect : public AssignmentEffect<AssignEffect> {
+public:
+  static constexpr char class_name[] = "assign";
 
-            virtual std::ostream& print(std::ostream& o) const {
-                o << "(" << Derived::class_name << " " <<
-                     *_function << " " << *_expression << ")";
-                return o;
-            }
+  typedef std::shared_ptr<AssignEffect> Ptr;
 
-            std::string print() const {
-                return Effect::print();
-            }
+  AssignEffect();
+  AssignEffect(const FunctionExpression::Ptr &function,
+               const Expression::Ptr &expression);
+  AssignEffect(const AssignEffect &other);
+  AssignEffect &operator=(const AssignEffect &other);
+  virtual ~AssignEffect();
+};
 
-        private :
-            FunctionExpression::Ptr _function;
-            Expression::Ptr _expression;
-        };
+class ScaleUpEffect : public AssignmentEffect<ScaleUpEffect> {
+public:
+  static constexpr char class_name[] = "scale-up";
 
+  typedef std::shared_ptr<ScaleUpEffect> Ptr;
 
-        class AssignEffect : public AssignmentEffect<AssignEffect> {
-        public :
-            static constexpr char class_name[] = "assign";
+  ScaleUpEffect();
+  ScaleUpEffect(const FunctionExpression::Ptr &function,
+                const Expression::Ptr &expression);
+  ScaleUpEffect(const ScaleUpEffect &other);
+  ScaleUpEffect &operator=(const ScaleUpEffect &other);
+  virtual ~ScaleUpEffect();
+};
 
-            typedef std::shared_ptr<AssignEffect> Ptr;
+class ScaleDownEffect : public AssignmentEffect<ScaleDownEffect> {
+public:
+  static constexpr char class_name[] = "scale-down";
 
-            AssignEffect() {}
+  typedef std::shared_ptr<ScaleDownEffect> Ptr;
 
-            AssignEffect(const FunctionExpression::Ptr& function,
-                         const Expression::Ptr& expression)
-                : AssignmentEffect<AssignEffect>(function, expression) {}
-            
-            AssignEffect(const AssignEffect& other)
-                : AssignmentEffect<AssignEffect>(other) {}
-            
-            AssignEffect& operator= (const AssignEffect& other) {
-                dynamic_cast<AssignmentEffect<AssignEffect>&>(*this) = other;
-                return *this;
-            }
+  ScaleDownEffect();
+  ScaleDownEffect(const FunctionExpression::Ptr &function,
+                  const Expression::Ptr &expression);
+  ScaleDownEffect(const ScaleDownEffect &other);
+  ScaleDownEffect &operator=(const ScaleDownEffect &other);
+  virtual ~ScaleDownEffect();
+};
 
-            virtual ~AssignEffect() {}
-        };
+class IncreaseEffect : public AssignmentEffect<IncreaseEffect> {
+public:
+  static constexpr char class_name[] = "increase";
 
+  typedef std::shared_ptr<IncreaseEffect> Ptr;
 
-        class ScaleUpEffect : public AssignmentEffect<ScaleUpEffect> {
-        public :
-            static constexpr char class_name[] = "scale-up";
+  IncreaseEffect();
+  IncreaseEffect(const FunctionExpression::Ptr &function,
+                 const Expression::Ptr &expression);
+  IncreaseEffect(const IncreaseEffect &other);
+  IncreaseEffect &operator=(const IncreaseEffect &other);
+  virtual ~IncreaseEffect();
+};
 
-            typedef std::shared_ptr<ScaleUpEffect> Ptr;
+class DecreaseEffect : public AssignmentEffect<DecreaseEffect> {
+public:
+  static constexpr char class_name[] = "decrease";
 
-            ScaleUpEffect() {}
+  typedef std::shared_ptr<DecreaseEffect> Ptr;
 
-            ScaleUpEffect(const FunctionExpression::Ptr& function,
-                          const Expression::Ptr& expression)
-                : AssignmentEffect<ScaleUpEffect>(function, expression) {}
-            
-            ScaleUpEffect(const ScaleUpEffect& other)
-                : AssignmentEffect<ScaleUpEffect>(other) {}
-            
-            ScaleUpEffect& operator= (const ScaleUpEffect& other) {
-                dynamic_cast<AssignmentEffect<ScaleUpEffect>&>(*this) = other;
-                return *this;
-            }
+  DecreaseEffect();
+  DecreaseEffect(const FunctionExpression::Ptr &function,
+                 const Expression::Ptr &expression);
+  DecreaseEffect(const DecreaseEffect &other);
+  DecreaseEffect &operator=(const DecreaseEffect &other);
+  virtual ~DecreaseEffect();
+};
 
-            virtual ~ScaleUpEffect() {}
-        };
-
-
-        class ScaleDownEffect : public AssignmentEffect<ScaleDownEffect> {
-        public :
-            static constexpr char class_name[] = "scale-down";
-
-            typedef std::shared_ptr<ScaleDownEffect> Ptr;
-
-            ScaleDownEffect() {}
-
-            ScaleDownEffect(const FunctionExpression::Ptr& function,
-                            const Expression::Ptr& expression)
-                : AssignmentEffect<ScaleDownEffect>(function, expression) {}
-            
-            ScaleDownEffect(const ScaleDownEffect& other)
-                : AssignmentEffect<ScaleDownEffect>(other) {}
-            
-            ScaleDownEffect& operator= (const ScaleDownEffect& other) {
-                dynamic_cast<AssignmentEffect<ScaleDownEffect>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~ScaleDownEffect() {}
-        };
-
-
-        class IncreaseEffect : public AssignmentEffect<IncreaseEffect> {
-        public :
-            static constexpr char class_name[] = "increase";
-
-            typedef std::shared_ptr<IncreaseEffect> Ptr;
-
-            IncreaseEffect() {}
-
-            IncreaseEffect(const FunctionExpression::Ptr& function,
-                           const Expression::Ptr& expression)
-                : AssignmentEffect<IncreaseEffect>(function, expression) {}
-            
-            IncreaseEffect(const IncreaseEffect& other)
-                : AssignmentEffect<IncreaseEffect>(other) {}
-            
-            IncreaseEffect& operator= (const IncreaseEffect& other) {
-                dynamic_cast<AssignmentEffect<IncreaseEffect>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~IncreaseEffect() {}
-        };
-
-
-        class DecreaseEffect : public AssignmentEffect<DecreaseEffect> {
-        public :
-            static constexpr char class_name[] = "decrease";
-
-            typedef std::shared_ptr<DecreaseEffect> Ptr;
-
-            DecreaseEffect() {}
-
-            DecreaseEffect(const FunctionExpression::Ptr& function,
-                           const Expression::Ptr& expression)
-                : AssignmentEffect<DecreaseEffect>(function, expression) {}
-            
-            DecreaseEffect(const DecreaseEffect& other)
-                : AssignmentEffect<DecreaseEffect>(other) {}
-            
-            DecreaseEffect& operator= (const DecreaseEffect& other) {
-                dynamic_cast<AssignmentEffect<DecreaseEffect>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~DecreaseEffect() {}
-        };
-
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
+
+#ifdef SKDECIDE_HEADERS_ONLY
+#include "impl/assignment_effect_impl.hh"
+#endif
 
 #endif // SKDECIDE_PDDL_ASSIGNMENT_EFFECT_HH
