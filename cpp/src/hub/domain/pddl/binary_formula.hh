023de2f10c6f72202ac573c69f29ce3f55f05e85
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-21 13:18:37 +0200

    Break up binary formula code

--- cpp/src/hub/domain/pddl/binary_formula.hh
+++ cpp/src/hub/domain/pddl/binary_formula.hh
@@ -9,59 +9,38 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        template <typename Derived>
-        class BinaryFormula : public Formula {
-        public :
-            typedef std::shared_ptr<BinaryFormula<Derived>> Ptr;
+template <typename Derived> class BinaryFormula : public Formula {
+public:
+  typedef std::shared_ptr<BinaryFormula<Derived>> Ptr;
 
-            BinaryFormula() {}
+  BinaryFormula();
+  BinaryFormula(const Formula::Ptr &left_formula,
+                const Formula::Ptr &right_formula);
+  BinaryFormula(const BinaryFormula<Derived> &other);
+  BinaryFormula<Derived> &operator=(const BinaryFormula<Derived> &other);
+  virtual ~BinaryFormula();
 
-            BinaryFormula(const Formula::Ptr& left_formula,
-                          const Formula::Ptr& right_formula)
-                : _left_formula(left_formula), _right_formula(right_formula) {}
-            
-            BinaryFormula(const BinaryFormula<Derived>& other)
-                : _left_formula(other._left_formula),
-                  _right_formula(other._right_formula) {}
-            
-            BinaryFormula<Derived>& operator= (const BinaryFormula<Derived>& other) {
-                this->_left_formula = other._left_formula;
-                this->_right_formula = other._right_formula;
-                return *this;
-            }
+  void set_left_formula(const Formula::Ptr &formula);
+  const Formula::Ptr &get_left_formula() const;
 
-            virtual ~BinaryFormula() {}
+  void set_right_formula(const Formula::Ptr &formula);
+  const Formula::Ptr &get_right_formula() const;
 
-            void set_left_formula(const Formula::Ptr& formula) {
-                _left_formula = formula;
-            }
+  virtual std::ostream &print(std::ostream &o) const;
 
-            const Formula::Ptr& get_left_formula() const {
-                return _left_formula;
-            }
+protected:
+  Formula::Ptr _left_formula;
+  Formula::Ptr _right_formula;
+};
 
-            void set_right_formula(const Formula::Ptr& formula) {
-                _right_formula = formula;
-            }
-
-            const Formula::Ptr& get_right_formula() const {
-                return _right_formula;
-            }
-
-            virtual std::ostream& print(std::ostream& o) const {
-                o << "(" << Derived::class_name << " " << *_left_formula << " " << *_right_formula << ")";
-                return o;
-            }
-
-        protected :
-            Formula::Ptr _left_formula;
-            Formula::Ptr _right_formula;
-        };
-
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
+
+#ifdef SKDECIDE_HEADERS_ONLY
+#include "impl/binary_formula_impl.hh"
+#endif
 
 #endif // SKDECIDE_PDDL_BINARY_FORMULA_HH
