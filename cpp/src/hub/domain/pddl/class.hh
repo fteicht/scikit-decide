5e5dbc4e77b1284f984289c260b4e85dc162c70a
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-21 13:44:00 +0200

    Break up class code

--- cpp/src/hub/domain/pddl/class.hh
+++ cpp/src/hub/domain/pddl/class.hh
@@ -10,47 +10,28 @@
 
 namespace skdecide {
 
-    namespace pddl {
-        
-        class Class : public Identifier,
-                      public FunctionContainer<Class> {
-        public :
-            static constexpr char class_name[] = "class";
+namespace pddl {
 
-            typedef std::shared_ptr<Class> Ptr;
-            typedef FunctionContainer<Class>::FunctionPtr FunctionPtr;
-            typedef FunctionContainer<Class>::FunctionSet FunctionSet;
-            
-            Class(const std::string& name)
-                : Identifier(name) {}
+class Class : public Identifier, public FunctionContainer<Class> {
+public:
+  static constexpr char class_name[] = "class";
 
-            Class(const Class& other)
-                : Identifier(other), FunctionContainer<Class>(other) {}
+  typedef std::shared_ptr<Class> Ptr;
+  typedef FunctionContainer<Class>::FunctionPtr FunctionPtr;
+  typedef FunctionContainer<Class>::FunctionSet FunctionSet;
 
-            Class& operator= (const Class& other) {
-                dynamic_cast<Identifier&>(*this) = other;
-                dynamic_cast<FunctionContainer<Class>&>(*this) = other;
-                return *this;
-            }
+  Class(const std::string &name);
+  Class(const Class &other);
+  Class &operator=(const Class &other);
+  virtual ~Class();
 
-            virtual ~Class() {}
+  std::ostream &print(std::ostream &o) const;
+};
 
-            std::ostream& print(std::ostream& o) const {
-                o << "(:class" << this->_name;
-                for (const auto& f : this->_container) {
-                    o << " " << *f;
-                }
-                o << ")";
-                return o;
-            }
-        };
+// Class printing operator
+std::ostream &operator<<(std::ostream &o, const Class &c);
 
-        // Class printing operator
-        inline std::ostream& operator<<(std::ostream& o, const Class& c) {
-            return c.print(o);
-        }
-
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
 
