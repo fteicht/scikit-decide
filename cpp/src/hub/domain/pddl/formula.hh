ad3479902f8263ccf82b357d820402b81b79e5b5
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-24 15:31:39 +0200

    Break up formula code

--- cpp/src/hub/domain/pddl/formula.hh
+++ cpp/src/hub/domain/pddl/formula.hh
@@ -11,28 +11,22 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        class Formula {
-        public :
-            typedef std::shared_ptr<Formula> Ptr;
+class Formula {
+public:
+  typedef std::shared_ptr<Formula> Ptr;
 
-            virtual ~Formula() {}
-            virtual std::ostream& print(std::ostream& o) const =0;
+  virtual ~Formula();
 
-            std::string print() const {
-                std::ostringstream o;
-                print(o);
-                return o.str();
-            }
-        };
+  virtual std::ostream &print(std::ostream &o) const = 0;
+  std::string print() const;
+};
 
-        // Formula printing operator
-        inline std::ostream& operator<<(std::ostream& o, const Formula& f) {
-            return f.print(o);
-        }
+// Formula printing operator
+std::ostream &operator<<(std::ostream &o, const Formula &f);
 
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
 
