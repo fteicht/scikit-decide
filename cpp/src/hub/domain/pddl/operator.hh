7e726ff106c8878d72f466ebc62b565e5f107db3
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-25 13:40:42 +0200

    Break up pddl operator code

--- cpp/src/hub/domain/pddl/operator.hh
+++ cpp/src/hub/domain/pddl/operator.hh
@@ -11,215 +11,119 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        template <typename Derived>
-        class Operator : public Identifier,
-                         public VariableContainer<Derived>,
-                         public BinaryEffect { // BinaryEffect brings in precondition and effect logics
-        public :
-            typedef std::shared_ptr<Operator<Derived>> Ptr;
-            typedef typename VariableContainer<Derived>::VariablePtr VariablePtr;
-            typedef typename VariableContainer<Derived>::VariableVector VariableVector;
+template <typename Derived>
+class Operator : public Identifier,
+                 public VariableContainer<Derived>,
+                 public BinaryEffect { // BinaryEffect brings in precondition
+                                       // and effect logics
+public:
+  typedef std::shared_ptr<Operator<Derived>> Ptr;
+  typedef typename VariableContainer<Derived>::VariablePtr VariablePtr;
+  typedef typename VariableContainer<Derived>::VariableVector VariableVector;
 
-            Operator(const std::string& name)
-                : Identifier(name) {}
-            
-            Operator(const std::string& name,
-                    const Formula::Ptr& precondition,
-                    const Effect::Ptr& effect)
-                : Identifier(name), BinaryEffect(precondition, effect) {}
+  Operator(const std::string &name);
+  Operator(const std::string &name, const Formula::Ptr &precondition,
+           const Effect::Ptr &effect);
+  Operator(const Operator<Derived> &other);
+  Operator<Derived> &operator=(const Operator<Derived> &other);
+  virtual ~Operator();
 
-            Operator(const Operator<Derived>& other)
-                : Identifier(other),
-                  VariableContainer<Derived>(other),
-                  BinaryEffect(other) {}
+  virtual std::ostream &print(std::ostream &o) const;
+};
 
-            Operator<Derived>& operator=(const Operator<Derived>& other) {
-                dynamic_cast<Identifier&>(*this) = other;
-                dynamic_cast<VariableContainer<Derived>&>(*this) = other;
-                dynamic_cast<BinaryEffect&>(*this) = other;
-                return *this;
-            }
+class Action : public Operator<Action> {
+public:
+  static constexpr char class_name[] = "action";
 
-            virtual ~Operator() {}
+  typedef std::shared_ptr<Action> Ptr;
+  typedef VariableContainer<Action>::VariablePtr VariablePtr;
+  typedef VariableContainer<Action>::VariableVector VariableVector;
 
-            virtual std::ostream& print(std::ostream& o) const {
-                o << "(:" << std::string(Derived::class_name) << ' ' << static_cast<const Derived*>(this)->get_name() << std::endl;
-                o << ":parameters (";
-                for (const auto & v : this->get_variables()) {
-                    o << " " << *v;
-                }
-                o << " )" << std::endl;
-                o << ":precondition " << *(this->get_condition()) << std::endl;
-                o << ":effect " << *(this->get_effect()) << std::endl;
-                o << ")";
-                return o;
-            }
-        };
+  Action(const std::string &name);
+  Action(const std::string &name, const Formula::Ptr &precondition,
+         const Effect::Ptr &effect);
+  Action(const Action &other);
+  Action &operator=(const Action &other);
+  virtual ~Action();
+};
 
-        class Action : public Operator<Action> {
-        public :
-            static constexpr char class_name[] = "action";
+// Action printing operator
+std::ostream &operator<<(std::ostream &o, const Action &a);
 
-            typedef std::shared_ptr<Action> Ptr;
-            typedef VariableContainer<Action>::VariablePtr VariablePtr;
-            typedef VariableContainer<Action>::VariableVector VariableVector;
+class DurativeAction : public Operator<DurativeAction> {
+public:
+  static constexpr char class_name[] = "durative-action";
 
-            Action(const std::string& name)
-                : Operator<Action>(name) {}
-            
-            Action(const std::string& name,
-                   const Formula::Ptr& precondition,
-                   const Effect::Ptr& effect)
-                : Operator<Action>(name, precondition, effect) {}
+  typedef std::shared_ptr<DurativeAction> Ptr;
+  typedef VariableContainer<DurativeAction>::VariablePtr VariablePtr;
+  typedef VariableContainer<DurativeAction>::VariableVector VariableVector;
 
-            Action(const Action& other)
-                : Operator<Action>(other) {}
+  DurativeAction(const std::string &name);
+  DurativeAction(const std::string &name,
+                 const Formula::Ptr &duration_constraint,
+                 const Formula::Ptr &precondition, const Effect::Ptr &effect);
+  DurativeAction(const DurativeAction &other);
+  DurativeAction &operator=(const DurativeAction &other);
+  virtual ~DurativeAction();
 
-            Action& operator=(const Action& other) {
-                dynamic_cast<Operator<Action>&>(*this) = other;
-                return *this;
-            }
+  void set_duration_constraint(const Formula::Ptr &duration_constraint);
+  const Formula::Ptr &get_duration_constraint() const;
 
-            virtual ~Action() {}
-        };
+  virtual std::ostream &print(std::ostream &o) const;
 
-        // Action printing operator
-        inline std::ostream& operator<<(std::ostream& o, const Action& a) {
-            return a.print(o);
-        }
+private:
+  Formula::Ptr _duration_constraint;
+};
 
+// Durative action printing operator
+std::ostream &operator<<(std::ostream &o, const DurativeAction &da);
 
-        class DurativeAction : public Operator<DurativeAction> {
-        public :
-            static constexpr char class_name[] = "durative-action";
+class Event : public Operator<Event> {
+public:
+  static constexpr char class_name[] = "event";
 
-            typedef std::shared_ptr<DurativeAction> Ptr;
-            typedef VariableContainer<DurativeAction>::VariablePtr VariablePtr;
-            typedef VariableContainer<DurativeAction>::VariableVector VariableVector;
+  typedef std::shared_ptr<Event> Ptr;
+  typedef VariableContainer<Event>::VariablePtr VariablePtr;
+  typedef VariableContainer<Event>::VariableVector VariableVector;
 
-            DurativeAction(const std::string& name)
-                : Operator<DurativeAction>(name) {}
-            
-            DurativeAction(const std::string& name,
-                           const Formula::Ptr& duration_constraint,
-                           const Formula::Ptr& precondition,
-                           const Effect::Ptr& effect)
-                : Operator<DurativeAction>(name, precondition, effect),
-                  _duration_constraint(duration_constraint) {}
+  Event(const std::string &name);
+  Event(const std::string &name, const Formula::Ptr &precondition,
+        const Effect::Ptr &effect);
+  Event(const Event &other);
+  Event &operator=(const Event &other);
+  virtual ~Event();
+};
 
-            DurativeAction(const DurativeAction& other)
-                : Operator<DurativeAction>(other),
-                  _duration_constraint(other._duration_constraint) {}
+// Event printing operator
+std::ostream &operator<<(std::ostream &o, const Event &e);
 
-            DurativeAction& operator=(const DurativeAction& other) {
-                dynamic_cast<Operator<DurativeAction>&>(*this) = other;
-                this->_duration_constraint = other._duration_constraint;
-                return *this;
-            }
+class Process : public Operator<Process> {
+public:
+  static constexpr char class_name[] = "process";
 
-            virtual ~DurativeAction() {}
+  typedef std::shared_ptr<Process> Ptr;
+  typedef VariableContainer<Process>::VariablePtr VariablePtr;
+  typedef VariableContainer<Process>::VariableVector VariableVector;
 
-            void set_duration_constraint(const Formula::Ptr& duration_constraint) {
-                _duration_constraint = duration_constraint;
-            }
+  Process(const std::string &name);
+  Process(const std::string &name, const Formula::Ptr &precondition,
+          const Effect::Ptr &effect);
+  Process(const Process &other);
+  Process &operator=(const Process &other);
+  virtual ~Process();
+};
 
-            const Formula::Ptr& get_duration_constraint() const {
-                return _duration_constraint;
-            }
+// Process printing operator
+std::ostream &operator<<(std::ostream &o, const Process &p);
 
-            virtual std::ostream& print(std::ostream& o) const {
-                o << "(:durative-action " << this->get_name() << std::endl;
-                o << ":parameters (";
-                for (const auto & v : get_variables()) {
-                    o << " " << *v;
-                }
-                o << " )" << std::endl;
-                o << ":duration " << *(this->get_duration_constraint()) << std::endl;
-                o << ":precondition " << *(this->get_condition()) << std::endl;
-                o << ":effect " << *(this->get_effect()) << std::endl;
-                o << ")";
-                return o;
-            }
-        
-        private :
-            Formula::Ptr _duration_constraint;
-        };
-
-        // Durative action printing operator
-        inline std::ostream& operator<<(std::ostream& o, const DurativeAction& da) {
-            return da.print(o);
-        }
-
-
-        class Event : public Operator<Event> {
-        public :
-            static constexpr char class_name[] = "event";
-
-            typedef std::shared_ptr<Event> Ptr;
-            typedef VariableContainer<Event>::VariablePtr VariablePtr;
-            typedef VariableContainer<Event>::VariableVector VariableVector;
-
-            Event(const std::string& name)
-                : Operator<Event>(name) {}
-            
-            Event(const std::string& name,
-                  const Formula::Ptr& precondition,
-                  const Effect::Ptr& effect)
-                : Operator<Event>(name, precondition, effect) {}
-
-            Event(const Event& other)
-                : Operator<Event>(other) {}
-
-            Event& operator=(const Event& other) {
-                dynamic_cast<Operator<Event>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~Event() {}
-        };
-
-        // Event printing operator
-        inline std::ostream& operator<<(std::ostream& o, const Event& e) {
-            return e.print(o);
-        }
-
-
-        class Process : public Operator<Process> {
-        public :
-            static constexpr char class_name[] = "process";
-
-            typedef std::shared_ptr<Process> Ptr;
-            typedef VariableContainer<Process>::VariablePtr VariablePtr;
-            typedef VariableContainer<Process>::VariableVector VariableVector;
-
-            Process(const std::string& name)
-                : Operator<Process>(name) {}
-            
-            Process(const std::string& name,
-                    const Formula::Ptr& precondition,
-                    const Effect::Ptr& effect)
-                : Operator<Process>(name, precondition, effect) {}
-
-            Process(const Process& other)
-                : Operator<Process>(other) {}
-
-            Process& operator=(const Process& other) {
-                dynamic_cast<Operator<Process>&>(*this) = other;
-                return *this;
-            }
-
-            virtual ~Process() {}
-        };
-
-        // Process printing operator
-        inline std::ostream& operator<<(std::ostream& o, const Process& p) {
-            return p.print(o);
-        }
-
-    } // namespace pddl
+} // namespace pddl
 
 } // namespace skdecide
+
+#ifdef SKDECIDE_HEADERS_ONLY
+#include "impl/operator_impl.hh"
+#endif
 
 #endif // SKDECIDE_PDDL_OPERATOR_HH
