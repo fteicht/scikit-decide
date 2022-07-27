9b2249fe4741965ccdd2868b3a340862020341eb
Florent Teichteil-Koenigsbuch <florent.teichteil@gmail.com>
2022-07-27 17:04:52 +0200

    Break up sequence container code

--- cpp/src/hub/domain/pddl/sequence_container.hh
+++ cpp/src/hub/domain/pddl/sequence_container.hh
@@ -15,107 +15,72 @@
 
 namespace skdecide {
 
-    namespace pddl {
+namespace pddl {
 
-        template <typename Derived, typename Symbol>
-        class SequenceContainer {
-        public :
-            SequenceContainer(const SequenceContainer& other)
-            : _container(other._container) {}
+class Variable;
 
-            SequenceContainer& operator=(const SequenceContainer& other) {
-                this->_container = other._container;
-                return *this;
-            }
-        
-        protected :
-            typedef std::shared_ptr<Symbol> SymbolPtr;
-            typedef std::vector<SymbolPtr> SymbolVector;
+template <typename Derived, typename Symbol> class SequenceContainer {
+public:
+  SequenceContainer(const SequenceContainer &other);
+  SequenceContainer &operator=(const SequenceContainer &other);
+  virtual ~SequenceContainer();
 
-            SymbolVector _container;
+protected:
+  typedef std::shared_ptr<Symbol> SymbolPtr;
+  typedef std::vector<SymbolPtr> SymbolVector;
 
-            SequenceContainer() {}
+  SymbolVector _container;
 
-            /**
-             * Appends a symbol to the container.
-             */
-            const SymbolPtr& append(const SymbolPtr& symbol) {
-                _container.push_back(symbol);
-                return _container.back();
-            }
+  SequenceContainer();
 
-            /**
-             * Appends a symbol to the container.
-             */
-            const SymbolPtr& append(const std::string& symbol) {
-                return append(std::make_shared<Symbol>(symbol));
-            }
+  /**
+   * Appends a symbol to the container.
+   */
+  const SymbolPtr &append(const SymbolPtr &symbol);
 
-            /**
-             * Removes all symbols matching the given one from the container.
-             */
-            void remove(const std::string& symbol) {
-                std::string lsymbol = StringConverter::tolower(symbol);
-                std::vector <typename SymbolVector::const_iterator> v;
-                for (typename SymbolVector::const_iterator i = _container.begin(); i != _container.end(); ++i) {
-                    if ((*i)->get_name() == lsymbol) {
-                        v.push_back(i);
-                    }
-                }
-                for (const auto& i : v) {
-                    _container.erase(i);
-                }
-            }
+  /**
+   * Appends a symbol to the container.
+   */
+  template <typename Tsymbol = Symbol,
+            typename std::enable_if_t<std::is_same<Tsymbol, Variable>::value,
+                                      int> = 0>
+  const SymbolPtr &append(const std::string &symbol);
 
-            /**
-             * Removes all symbols matching the given one from the container.
-             */
-            void remove(const SymbolPtr& symbol) {
-                remove(symbol->get_name());
-            }
+  /**
+   * Removes all symbols matching the given one from the container.
+   */
+  void remove(const std::string &symbol);
 
-            /**
-             * Gets all the symbols matching the given one from the container
-             */
-            SymbolVector get(const std::string& symbol) const {
-                std::string lsymbol = StringConverter::tolower(symbol);
-                SymbolVector v;
-                for (typename SymbolVector::const_iterator i = _container.begin(); i != _container.end(); ++i) {
-                    if ((*i)->get_name() == lsymbol) {
-                        v.push_back(*i);
-                    }
-                }
-                return v;
-            }
+  /**
+   * Removes all symbols matching the given one from the container.
+   */
+  void remove(const SymbolPtr &symbol);
 
-            /**
-             * Gets all the symbols matching the given one from the container
-             */
-            SymbolVector get(const SymbolPtr& symbol) const {
-                return get(symbol->get_name());
-            }
+  /**
+   * Gets all the symbols matching the given one from the container
+   */
+  SymbolVector get(const std::string &symbol) const;
 
-            /**
-             * Gets a symbol from the container at a given index
-             * Throws an exception if the given index exceeds the container size
-             */
-            const SymbolPtr& at(const std::size_t& index) const {
-                if (index >= _container.size()) {
-                    throw std::out_of_range("SKDECIDE exception: index " + std::to_string(index) +
-                                            " exceeds the size of the vector of " + std::string(Symbol::class_name) + "s of " +
-                                            std::string(Derived::class_name) + " '" + static_cast<const Derived*>(this)->get_name() + "'");
-                } else {
-                    return _container[index];
-                }
-            }
+  /**
+   * Gets all the symbols matching the given one from the container
+   */
+  SymbolVector get(const SymbolPtr &symbol) const;
 
-            const SymbolVector& get_container() const {
-                return _container;
-            }
-        };
+  /**
+   * Gets a symbol from the container at a given index
+   * Throws an exception if the given index exceeds the container size
+   */
+  const SymbolPtr &at(const std::size_t &index) const;
 
-    } // namespace pddl
+  const SymbolVector &get_container() const;
+};
+
+} // namespace pddl
 
 } // namespace skdecide
+
+#ifdef SKDECIDE_HEADERS_ONLY
+#include "impl/sequence_container_impl.hh"
+#endif
 
 #endif // SKDECIDE_PDDL_SEQUENCE_CONTAINER_HH
